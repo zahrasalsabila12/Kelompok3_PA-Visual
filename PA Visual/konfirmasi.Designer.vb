c211a83734e2cdf059f75b4dc0091974acb9ed80
Mohammad Fhadil <hafidsfhadil@gmail.com>
2023-05-22 17:01:39 +0800

    konfirmasi

--- PA Visual/konfirmasi.Designer.vb
+++ PA Visual/konfirmasi.Designer.vb
@@ -33,9 +33,11 @@ Partial Class konfirmasi
         Me.GroupBox1.BackColor = System.Drawing.Color.SeaShell
         Me.GroupBox1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.GroupBox1.ForeColor = System.Drawing.Color.SaddleBrown
-        Me.GroupBox1.Location = New System.Drawing.Point(284, 80)
+        Me.GroupBox1.Location = New System.Drawing.Point(213, 65)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(658, 209)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(494, 170)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Keranjang Anda"
@@ -45,9 +47,10 @@ Partial Class konfirmasi
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Arial", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(280, 40)
+        Me.Label1.Location = New System.Drawing.Point(210, 32)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(400, 19)
+        Me.Label1.Size = New System.Drawing.Size(356, 16)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Silahkan periksa kembali barang belanjaan anda ;)"
         '
@@ -56,9 +59,10 @@ Partial Class konfirmasi
         Me.Button1.BackColor = System.Drawing.Color.RosyBrown
         Me.Button1.Font = New System.Drawing.Font("Arial", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.SystemColors.HighlightText
-        Me.Button1.Location = New System.Drawing.Point(375, 336)
+        Me.Button1.Location = New System.Drawing.Point(281, 273)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(170, 49)
+        Me.Button1.Size = New System.Drawing.Size(128, 40)
         Me.Button1.TabIndex = 4
         Me.Button1.Text = "Kembali untuk mengubah"
         Me.Button1.UseVisualStyleBackColor = False
@@ -68,26 +72,28 @@ Partial Class konfirmasi
         Me.Button2.BackColor = System.Drawing.Color.RosyBrown
         Me.Button2.Font = New System.Drawing.Font("Arial", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.ForeColor = System.Drawing.SystemColors.HighlightText
-        Me.Button2.Location = New System.Drawing.Point(671, 336)
+        Me.Button2.Location = New System.Drawing.Point(503, 273)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(170, 49)
+        Me.Button2.Size = New System.Drawing.Size(128, 40)
         Me.Button2.TabIndex = 5
         Me.Button2.Text = "Checkout"
         Me.Button2.UseVisualStyleBackColor = False
         '
         'konfirmasi
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.PA_Visual.My.Resources.Resources.konfirmasi
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(963, 450)
+        Me.ClientSize = New System.Drawing.Size(722, 366)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.GroupBox1)
         Me.DoubleBuffered = True
         Me.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "konfirmasi"
         Me.Text = "konfirmasi"
         Me.ResumeLayout(False)
